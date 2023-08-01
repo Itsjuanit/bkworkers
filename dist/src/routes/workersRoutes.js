3d2b6d2434d0e40f2c9672275360a05d600e104f
pbottino <pbottino95@gmail.com>
2023-08-01 17:12:05 -0300

    workers route jwt

--- dist/src/routes/workersRoutes.js
+++ dist/src/routes/workersRoutes.js
@@ -5,10 +5,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const workers_1 = require("../controllers/workers");
+const validate_jwt_1 = require("../middlewares/validate-jwt");
 const router = express_1.default.Router();
 router.get('/', workers_1.getWorker);
-router.post('/', workers_1.createWorker);
-router.put('/:id', workers_1.updateWorker);
-router.patch('/:id', workers_1.updateStatus);
+router.post('/', [validate_jwt_1.validateJWT], workers_1.createWorker);
+router.put('/:id', [validate_jwt_1.validateJWT], workers_1.updateWorker);
+router.patch('/:id', [validate_jwt_1.validateJWT], workers_1.updateStatus);
 exports.default = router;
 //# sourceMappingURL=workersRoutes.js.map
--- src/routes/workersRoutes.ts
+++ src/routes/workersRoutes.ts
@@ -1,12 +1,13 @@
 import express from 'express'
 import { createWorker, getWorker, updateStatus, updateWorker } from '../controllers/workers'
+import { validateJWT } from '../middlewares/validate-jwt'
 
 
 const router = express.Router()
 
 router.get('/', getWorker)
-router.post('/', createWorker)
-router.put('/:id', updateWorker)
-router.patch('/:id', updateStatus)
+router.post('/', [validateJWT], createWorker)
+router.put('/:id',[validateJWT], updateWorker)
+router.patch('/:id',[validateJWT], updateStatus)
 
 export default router
